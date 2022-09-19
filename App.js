9b5d3cee77d056392a6fbb8715f738e0ea7c7bfd
Fernanda Brum <fernanda.brumlousada@gmail.com>
2022-09-19 18:19:23 -0300

    feat: interface and some functionalities

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import Button from './src/components/Button';
+import Display from './src/components/Display';
+
+const App: () => Node = () => {
+  const [displayValue, setDisplayValue] = useState('0')
+
+  const addDigit = n => setDisplayValue(n)
+  const clearMemory = () => setDisplayValue('0')
+  const setOperation = op => {} 
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Display value={displayValue} />
+      <View style={styles.buttons}>
+        <Button label='AC' triple onClick={clearMemory} /> 
+        <Button label='/' operation onClick={setOperation} /> 
+        <Button label='7' onClick={addDigit} />
+        <Button label='8' onClick={addDigit} />
+        <Button label='9' onClick={addDigit} />
+        <Button label='*' operation onClick={setOperation} />
+        <Button label='4' onClick={addDigit} />
+        <Button label='5' onClick={addDigit} />
+        <Button label='6' onClick={addDigit} />
+        <Button label='-' operation onClick={setOperation} />
+        <Button label='1' onClick={addDigit} />
+        <Button label='2' onClick={addDigit} />
+        <Button label='3' onClick={addDigit} />
+        <Button label='+' operation onClick={setOperation} />
+        <Button label='0' double onClick={addDigit} />
+        <Button label='.' onClick={addDigit} />
+        <Button label='=' operation onClick={setOperation} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
+});
+
+export default App;
